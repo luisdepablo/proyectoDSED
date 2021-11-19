c9355a9b356204aabc5e17368fc04c67fad28155
luisdepablo <47429125+luisdepablo@users.noreply.github.com>
2021-11-19 13:07:27 +0100

    V1.1

--- final_project/final_project.srcs/sources_1/new/en_4_cycles.vhd
+++ final_project/final_project.srcs/sources_1/new/en_4_cycles.vhd
@@ -34,8 +34,15 @@ end en_4_cycles;
 
 architecture Behavioral of en_4_cycles is
 
-signal r_reg,r_next: unsigned(2 downto 0) := "000";
+signal r_reg_2,r_next_2: unsigned(2 downto 0) := "000";
 signal en_2_cycles_aux: std_logic;
+
+signal r_reg_4,r_next_4: unsigned(2 downto 0) := "000";
+signal en_4_cycles_aux: std_logic;
+
+signal r_reg_3,r_next_3: unsigned(2 downto 0) := "000";
+signal en_3megas: std_logic;
+signal clk3_meg:std_logic:='0';
 
 
 
@@ -45,33 +52,77 @@ begin
     process(clk_12megas,reset)
     begin
         if(reset='1') then
-            r_reg<=(others=>'0');
+            r_reg_2<=(others=>'0');
+            r_reg_4<=(others=>'0');
+            r_reg_3<=(others=>'0');
         elsif(rising_edge(clk_12megas)) then
-            r_reg<=r_next;
+            r_reg_2<=r_next_2;
+            r_reg_4<=r_next_4;
+            r_reg_3<=r_next_3;
         end if;
     end process;
                   
 --next state logic
-    process(r_next,r_reg)
+    process(r_next_2,r_reg_2)
     begin
-        if (r_reg = 1) then 
-            r_next <= (others => '0');
+        if (r_reg_2 = 1) then 
+            r_next_2 <= (others => '0');
           else     
-          r_next <=  r_reg +1;   
+          r_next_2 <=  r_reg_2 +1;   
         end if;    
     end process;
     
 
 --output logic
-process(r_next,r_reg,en_2_cycles_aux)
+process(r_next_2,r_reg_2,en_2_cycles_aux)
     begin
-        if (r_reg = 1) then 
+        if (r_reg_2 = 1) then 
            en_2_cycles_aux <= '1';
           else     
            en_2_cycles_aux<=  '0';  
         end if;    
     end process;
 en_2_cycles<= en_2_cycles_aux;
+
+--CIRCUIT FOR EN_4_CICLES
+--next state logic
+    process(r_next_4,r_reg_4)
+    begin
+        if (r_reg_4 = 4) then 
+            r_next_4 <= (others => '0');
+          else     
+          r_next_4 <=  r_reg_4 +1;   
+        end if;    
+    end process;
+    
+
+--output logic
+process(r_next_4,r_reg_4,en_4_cycles_aux)
+    begin
+        if (r_reg_4 = 2) then 
+           en_4_cycles_aux <= '1';
+          else     
+           en_4_cycles_aux<=  '0';  
+        end if;    
+    end process;
+en_4_cycles<= en_4_cycles_aux;
+
+--CIRCUIT FOR CLK3MHZ
+--next state logic
+    process(r_next_3,r_reg_3)             
+    begin
+                                        
+        if (r_reg_3 = 3) then             
+            r_next_3 <= (others => '0');
+             
+          else
+                              
+          r_next_3 <=  r_reg_3 +1;        
+        end if;                           
+    end process;                          
+
+--output logic
+clk_3megas<= '1' when (r_reg_3>1 and r_reg_3<=3) else '0';
 
 
 end Behavioral;
