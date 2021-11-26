1b930ab844119bd3d55cb96ba5232cd0e6f58a20
vicpt99 <vicpt99@gmail.com>
2021-11-26 19:41:47 +0100

    V.FSMD
    falta terminarlo

--- final_project/final_project.srcs/sources_1/imports/new/FSMD_microphone.vhd
+++ final_project/final_project.srcs/sources_1/imports/new/FSMD_microphone.vhd
@@ -45,9 +45,10 @@ end FSMD_microphone;
 architecture Behavioral of FSMD_microphone is
 
 type fsmd_state_type is (idle,inicio);
-signal count, data_1,data_2,first_cycle : unsigned (sample_size-1 downto 0);
-signal count_next, data_1_next,data_2_next,first_cycle_next : unsigned (sample_size-1 downto 0);
+signal count, data_1,data_2: unsigned (sample_size-1 downto 0);
+signal count_next, data_1_next,data_2_next : unsigned (sample_size-1 downto 0);
 signal state_reg,state_next: fsmd_state_type;
+signal first_cycle,first_cycle_next : std_logic;
 begin
 
 --state and data register
@@ -59,41 +60,55 @@ begin
              count, data_1,data_2,first_cycle,
              count_next, data_1_next,data_2_next,first_cycle_next)
     begin
-    case state_reg is
-    when idle =>
-            count_next<= (others=>'0');
-            data_1_next<= (others=>'0');
-            data_2_next<= (others=>'0');
-            first_cycle_next<= (others=>'0');
-            state_next<= inicio;
-            
-    when inicio=>
-             if (reset ='1') then
-                    state_next<= idle;
-             elsif ((count>=0 and count<= 105) or(count>=150 and count<= 255) ) then
-                    count_next<= count + 1;
-                    if(micro_data='1') then
-                        data_1_next<= data_1 + 1;
-                        data_2_next<= data_2 + 1;
-                    end if;
-                    state_next<= inicio;
-                elsif((count>=106 and count<= 149)) then
-                    count_next <= count + 1;
+    case state_reg is        
+        when idle=>
+            if (reset ='1') then
+                count_next<= (others=>'0');
+                data_1_next<= (others=>'0');
+                data_2_next<= (others=>'0');
+                first_cycle_next<= (others=>'0');
+                state_next<= idle;
+            elsif ((count>=0 and count<= 105) or(count>=150 and count<= 255) ) then
+                count_next<= count + 1;
+                if(micro_data='1') then
+                    data_1_next<= data_1 + 1;
+                    data_2_next<= data_2 + 1;
+                end if;
+                state_next<= idle;
+            elsif((count>=106 and count<= 149)) then
+                count_next <= count + 1;
                     if (micro_data='1') then
                         data_1_next <= data_1 + 1; 
                     end if;
-                    if ((first_cycle ='1') and count = 106)then
+                    if ((first_cycle =1) and count = 106)then
                         data_2_next<= (others=>'0');
-                        sample_out <= data_2;
-                        sample_out_ready <= enable_4_cycles;                   
+                        sample_out <= std_logic_vector(data_2);
+                        --sample_out_ready <= enable_4_cycles;                   
                     else
-                        --sample_out_ready <=(others=>'0');
+                        sample_out_ready <='0';
                     end if;
-                        state_next<= inicio;
-                    elsif( micro_data='1') then
-                        data_2_next <= data_2 + 1;
-                    end if;
-         
-             
-       end case;     
+                    state_next<= inicio;
+            else
+                if( micro_data='1') then
+                    data_2_next <= data_2 + 1;
+                end if;
+                if (count =299) then
+                    count_next<=(others=>'0');
+                    first_cycle_next=> '1';
+                    --sample_out_ready<='0';
+                    state_next<= idle;
+                else
+                    count_next <= count + 1;
+                    if(count =256) then
+                        sample_out<=std_logic_vector(data_1);
+                        data_1_next <= (others=>'0');
+                        --sample_out_ready=> enable_4_cycles;
+                    else
+                        sample_out_ready<='0';
+                end if;
+                state_next<= idle;
+                         
+        end case;     
+        
+        
 end Behavioral;
